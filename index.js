b22e1d007a21e85cd915a75b22bdaf64e0adbb7d
Farolan Faisal <farolanfaisal@gmail.com>
2022-08-18 11:45:30 +0000

    add contact page

--- index.js
+++ index.js
@@ -18,8 +18,23 @@ function onClickContact(e) {
   scrollToElement('#contact');
 }
 
+function onSubmitContact(e) {
+  console.log('submit')
+  e.preventDefault();
+  alert('Your form has been sent!');
+  window.location = '/';
+}
+
 window.onload = function() {
-  document.querySelector('#home-btn').addEventListener('click', onClickHome);
-  document.querySelector('#services-btn').addEventListener('click', onClickServices);
-  document.querySelector('#contact-btn').addEventListener('click', onClickContact);
+  var homeBtn = document.querySelector('#home-btn');
+  homeBtn && homeBtn.addEventListener('click', onClickHome);
+  
+  var servicesBtn = document.querySelector('#services-btn');
+  servicesBtn && servicesBtn.addEventListener('click', onClickServices);
+
+  var contactBtn = document.querySelector('#contact-btn');
+  contactBtn && contactBtn.addEventListener('click', onClickContact);
+
+  var contactForm = document.querySelector('#contact-form');
+  contactForm.addEventListener('submit', onSubmitContact);
 }
